8776afeb90bc825e5ce6378065a0b24ebe21c30c
Paweł Mikłas <pablomiklas@gmail.com>
2020-10-05 20:51:49 +0200

    operators

---
+++ 3.Operatory/script.js
@@ -0,0 +1,59 @@
+// Zadanie 1
+
+const truthyValue = true;
+const falsyValue = false;
+
+console.log(truthyValue === falsyValue);
+
+// Zadanie 2
+
+const firstVariable = 10;
+const secondVariable = 5;
+let moduloResult = 0;
+
+moduloResult = firstVariable % secondVariable;
+
+console.log('moduloResult', moduloResult);
+
+// Zadanie 3
+
+const sampleString = '1lorem';
+const sampleSecondString = 'tytus';
+let stringResults = '';
+
+stringResults = sampleString + sampleSecondString;
+
+console.log('stringResults', stringResults);
+
+// Zadanie 4
+
+var someNumber = 425;
+var someString = "425";
+
+console.log('someNumber == someString', someNumber == someString);
+console.log('someNumber === someString', someNumber === someString);
+
+// operator === porównuje typy dlatego wartość jest równa false
+
+// Zadanie 5
+
+let counter = 30;
+
+counter++;
+
+console.log('30', counter);
+
+counter--;
+
+console.log('30', counter);
+
+// Zadanie 6
+
+const sampleNumber = 123;
+const sampleSecondNumber = 43543;
+let result = null;
+
+result = sampleNumber > sampleSecondNumber;
+
+console.log('result', result);
+
