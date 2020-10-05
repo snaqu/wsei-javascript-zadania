de00ed291497a3a985a2b5305614202f88ae1300
Paweł Mikłas <pablomiklas@gmail.com>
2020-10-05 19:53:30 +0200

    second

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,25 @@
+// Zadanie 1
+
+const sampleNumber = 1;
+const sampleString = 'string';
+const sampleNumberWithLetter = 1 + 'l';
+const sampleLogicValue = 10 > 20;
+const sampleSpecialValue = null;
+
+// Zadanie 2
+
+const first = 10;
+const second = 20;
+let sum = 0;
+
+const result = first + second;
+
+sum = result;
+
+// Zadanie 3
+
+let sampleVariable;
+
+console.log(sampleVariable);
+
+// undefined - zmienna nie ma przypisanej wartości
